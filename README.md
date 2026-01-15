9ec20903bd199ddce67f77011a065d720120bacb
Autism-Prime <tim.gostisa07@gmail.com>
2026-01-15 17:35:04 +0000

    Vaje

--- README.md
+++ README.md
@@ -1,1 +1,47 @@
-# Uvod-v-OSA
+# Uvod-v-OSA# komentar
+"""
+to
+je
+večvrstični komentar
+"""
+print("hello world")
+# integrs cela števila
+x = 5
+y = -10
+z = 100000000
+
+vosta = x + y + 20
+print(x+y)
+odst = x -y
+mnoz = x * y
+delj = x / y
+celoDelj = 10//3
+potence = 10**3
+deljenjeZostankom = 10 % 3
+
+# float - decimalna števila
+
+pi = 3.14
+
+print ( pi + 10 )
+
+# String ali nizi znakov
+
+ime = "Tim"
+starost = "18"
+
+print (ime)
+print (starost)
+print (ime + starost)
+print (ime * 100)
+
+#indeksi
+#altgr f, g
+print (ime [0])
+print (ime [1])
+print (ime [2])
+print (ime.upper())
+print (ime.lower())
+print (ime.strip()) #odstrani whitespace torej presledke
+print (ime.join()) #združuje 
+
---
+++ main.py
@@ -0,0 +1,49 @@
+# koliko bom star čez 10 let
+
+#starost = int(input("koliko si star?"))
+#cezDeset = starost + 10
+#print(type(starost)
+
+#altgr + b,n
+#print(f"danes si { starost} , čez 10 let boš  {cezDeset}")
+
+
+# Napiši program ki prebere kepic sladoleda in ceno kepce sladoleda ter izračuna ceno sladoleda
+
+# 10 kepic sladoleda po 3eur je 30eur
+
+#štKepic = int(input("koliko kepic želite?"))
+#cenaKepic = float(input("cena ene kepice"))
+
+#skupaj = štKepic * cenaKepic
+#print (f"zbrali ste {štKepic} kepic ki stanejo {cenaKepic}eur plačali boste {skupaj}eur")
+
+#vaje
+# naloga 1
+
+#cenaIzdelka = 100
+#popust = 20
+#popD = 1-(pop/100)
+
+#končnaCena = pop
+#print (f"izdelek za s popustom  stane {končnaCena}")
+
+#vaja 2
+
+minute = 10
+sekunde = 20
+skupaj = minute * 60 + sekunde
+print(skupaj)
+
+
+# vaja 3
+
+celzije = 30
+vFarn = celzije * 1.8 + 32
+
+# vaja 4
+
+ure = 20
+postavka = 20
+
+placa = ure*postavka
