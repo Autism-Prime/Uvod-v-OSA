83c4d7414bb52776fe9178c82e29b4935e902497
Autism-Prime <tim.gostisa07@gmail.com>
2026-01-29 16:38:01 +0000

    predavanje 2

---
+++ 2ura.py
@@ -0,0 +1,43 @@
+#spremenljivke
+
+##password: "12789"
+
+#print
+#print(starost, password, sep =" & ")
+
+# seznami / list
+
+# povprecje visin dijakov v razredu 
+
+visine = [180, 178, 160, 177, 187, 188] # seznam celih stevil / intiger list
+print(visine)
+# dostop do indeksev seznama
+print(visine[0])
+print(visine[5])
+print(len(visine))
+print(max(visine))
+print(min(visine))
+
+povprecje = sum(visine)  / len(visine)
+print(povprecje)
+
+ime = "aleksander veliki"
+#ime = "A", "l", 
+print(ime[0])
+
+
+
+# pogojni stavki / if statement
+
+#izpisi ce je oseba polnoletna ali ni
+
+starost = 15
+# starost = input("vensi svojo starost")
+#if pogoj:
+#    koda 1
+#    koda 2
+# koda 3
+if starost >= 18:
+    print("oseba je polnoletna")
+else:
+    print("oseba ni polnoletna")
